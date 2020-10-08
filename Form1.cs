b92643ba04cbe287fc77fb80e81e3e763e4f9a33
Bionoob <bigjarik@gmail.com>
2020-10-08 22:42:51 +0300

    hello

--- Form1.cs
+++ Form1.cs
@@ -1,38 +1,41 @@
-﻿using Newtonsoft.Json;
+﻿using HtmlAgilityPack;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TeleSharp.TL;
+using TLSharp.Core;
 
 namespace Schoolparse
 {
     public partial class Form1 : Form
     {
         static string ExPath = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\alarm.wav";
+        System.Media.SoundPlayer smm = new System.Media.SoundPlayer();
+        WebClient wc = new WebClient();
         public Form1()
         {
             InitializeComponent();
-        }
-        private void button1_Click(object sender, EventArgs e)
-        {
-            WebClient wc = new WebClient();
-            System.Media.SoundPlayer smm = new System.Media.SoundPlayer();
             smm.SoundLocation = ExPath;
             smm.LoadAsync();
             wc.Encoding = System.Text.Encoding.UTF8;
-            wc.Headers.Add(HttpRequestHeader.Cookie, "__RequestVerificationToken=mua0s4X3GtpDs-Y9l4c4Sc-3-nWpBVImC8gjaKVGYNzPq3NRYU5HkZcaAMNYDRYtheg1xCFd_l1JU9njtxLvxsSiaLVMgg_jf3GBalWdTMs1; supportOnlineTalkID=WXwwZgW2wQ3Rbq9QkOEyx3Hl77VGsyxW; _ym_uid=1602165385308781638; _ym_d=1602165385; _ym_isad=1; _ym_visorc_39917430=w; .AspNet.ApplicationCookie=G6lVJUeywtPBYxok5L82wyRYVFdgS5b00Z5tuzZ9XrmyouQmvZrvDGKIbxuw-NDbohU_eKhXiURaP-N2kAoDMZHg_brWIO9pvVtmDuXJPdR3d4-OBzpJFIBywSLZ5kLF8vBxdVn1gdPoJKU1vZZ0pK56ZW8Mqw4DqwyAN9HgcM1M4YdwigTyIpEsB2hX9pJmqHUfyF372sMOAi2aK4JniWoQhq7T9FVA_qNfNaW1w4aM0Zh2VVTtBysDDQY9mmWO9E69uFV3aq5EW1dc72p0byz7iKGM6eS7khaPGUyzhB0R2hOyJ4-e6DVukfNSiqPBPTuKydwzk8az-JZ8MfMu76bAb0KnRNdLtvQfGEGRcUf0tvO8APSXJF_IxFZb3kw7glFb1bBpGqAQshmG7jHPY1XCn6Pj9oB7q5QixL8zoYPNtryqPbz3oOTDV4ontRT5T1tCZfG9dtVusgLXKA59f2ltln2CuUv6d51Qq45WchN-yEKIp-rhPaIDUQ1l5a_0A-V9Z_N9Qc34LHa4qATJOgxAXnRnse65PLokGTMMrqgm-TkqX0Ix0PSUpVdGQc_vvBQn2aSbVwW80gBWD0u0kDI6lPt35Mgb79ocYdjTld2VIBLJPMcrX5_mBVxOr3FDVzNgsZAx5DCe0H2X6Tw-ZLdrgmYrRpsKxgieBTVSWhynAJvCm1IXyp3xDNuLU3Xccf70RyPSef34SdGxnkKe8dq3LOmhK1Va1rGTuFEFMOUxQvCSKziIzBTNH-kLA20Ygb4omBDR5K0WyUgLDYDYD6f6wzbndiia_3Wa_Swssm8M-etsHU2Oba4na5Rb_aeo3VVTFLawo2ZXRnF4pNrP9Ql1DTMRehpjWnMvjQLarPjkztQlRcDK2irCApn4Ent52R3K6Mc4Ftydqut7XrF3TQ");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
             int i = 0;
             Task.Run(async () =>
             {
-                var z = wc.DownloadString("https://app.dscontrol.ru/content/deploy/app.js?1602083001025");
                 while (true)
                 {
                     i++;
                     var t = wc.DownloadString("https://app.dscontrol.ru/Api/StudentSchedulerList?Kinds=D&OnlyMine=falsetimeshift=-360&from=2020-10-01&to=2020-10-30");
-                    var zs = JsonConvert.DeserializeObject<Data>(t);
+                    var zs = JsonConvert.DeserializeObject<DataCalender>(t);
                     var bb = zs.data.Where(q => q.Completed != true && q.start_date.DayOfYear > DateTime.Now.DayOfYear).ToList();
                     if (bb.Count > 0)
                     {
@@ -48,7 +51,7 @@ namespace Schoolparse
                             listBox1.Items.Add(item);
                         }
                         counter_lbl.Text = i.ToString();
-                        if(bb.Count > 0)
+                        if (bb.Count > 0)
                         {
                             listBox2.Items.Clear();
                             foreach (var item in bb)
@@ -57,17 +60,86 @@ namespace Schoolparse
                             }
                         }
                     }));
-
                     GC.Collect(1, GCCollectionMode.Forced);
                     await Task.Delay(60001);
                 }
             });
 
         }
+        private void login_btn_Click(object sender, EventArgs e)
+        {
+            wc.Headers.Clear();
+            var request = (HttpWebRequest)WebRequest.Create("https://app.dscontrol.ru/Login");
+            var postData = "Login=43339046";
+            postData += "&TextPassword=";
+            postData += "&Password=FVAC%24";
+            postData += "&PreventPass=false";
+            var data = Encoding.ASCII.GetBytes(postData);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+            request.Accept = "*/*";
+            request.Referer = "https://app.dscontrol.ru/login";
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+            var response = (HttpWebResponse)request.GetResponse();
+            var aa = response.Headers["Set-Cookie"].Split(';')[0];
+            var za = wc.DownloadString("https://app.dscontrol.ru/Api/StudentSchedulerList?Kinds=D&OnlyMine=falsetimeshift=-360&from=2020-10-01&to=2020-10-30");
+            HtmlAgilityPack.HtmlDocument hd = new HtmlAgilityPack.HtmlDocument();
+            hd.LoadHtml(za);
+            HtmlNode formNode = hd.DocumentNode.SelectSingleNode("//input[@name='__RequestVerificationToken']");
+            var signupFormId = formNode.GetAttributeValue("value", "");
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            var abb = $"__RequestVerificationToken={signupFormId}; {aa};";
+            wc.Headers.Add(HttpRequestHeader.Cookie, abb);
+            var plochadki = wc.DownloadString("https://app.dscontrol.ru/api/AutodromeList");
+            var plochadki_class = JsonConvert.DeserializeObject<DataPlace>(plochadki);
+        }
+        public static TelegramClient tc;
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            tc = new TelegramClient(1929836, "71b59f552c59293894b11ee9479ff72f");
+            await tc.ConnectAsync();
+           
+            if(!tc.IsUserAuthorized())
+            {
+                var hash = await tc.SendCodeRequestAsync("+79162206436");
+            }
+            //else
+            //{
+            //    telega_code_txt.Text = "UUUUU";
+            //}
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            var code = telega_code_txt.Text; // you can change code in debugger
+            var user = await tc.MakeAuthAsync("+79162206436", "71b59f552c59293894b11ee9479ff72f", code);
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            var result = await tc.GetContactsAsync();
+
+            //find recipient in contacts
+            var user = result.Users
+                .Where(x => x.GetType() == typeof(TLUser))
+                .Cast<TLUser>()
+                .FirstOrDefault(x => x.Phone == "+79162206436");
+
+            //send message
+            await tc.SendMessageAsync(new TLInputPeerUser() { UserId = user.Id }, "HELLO MA FACKA FROM PC");
+        }
     }
-    public class Data
+    public class DataCalender
     {
         public List<ItemSc> data { get; set; }
+    }
+    public class DataPlace
+    {
+        public List<ItemPlace> data { get; set; }
     }
     public class ItemSc
     {
@@ -82,6 +154,15 @@ namespace Schoolparse
         public override string ToString()
         {
             return $"{EmployeeName} :: {start_date}";
+        }
+    }
+    public class ItemPlace
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public override string ToString()
+        {
+            return $"{Id} :: {Name}";
         }
     }
 }
